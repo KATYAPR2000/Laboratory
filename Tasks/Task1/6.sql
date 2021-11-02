02e5633fecb3ad5703533c67ad8a7c3f190c9e77
KATYAPR2000 <90416983+KATYAPR2000@users.noreply.github.com>
2021-11-02 12:28:35 +0300

    Update and rename Tasks/Task1/6.sql to Задачи/Задача 1/6.sql

--- Tasks/Task1/6.sql
+++
@@ -1,7 +0,0 @@
---6. ѕоказать пол€ ProductID, [Name], ProductSubcategoryID 
---из таблицы Production.Product, где идентификационный номер подкатегории 
---(ProductSubcategoryID) равен или 1, или 2, или 3, или 4, или 5.
---»спользовать оператор IN:
-SELECT ProductID, [Name], ProductSubcategoryID
-FROM Production.Product
-WHERE ProductSubcategoryID IN (1, 2, 3, 4, 5);
