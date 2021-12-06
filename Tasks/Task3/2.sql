771b3702c61a70daf64245ecba16291092cedad7
Прокопьева Екатерина Евгеньевна <katyaprokopyeva2000@mail.ru>
2021-12-06 21:13:15 +0300

    Лабораторная работа №3 #11

---
+++ Tasks/Task3/2.sql
@@ -0,0 +1,9 @@
+﻿--Показать список ID работников (BusinessEntityID), в котором указано имеет 
+--работник резюме или нет, из таблиц HumanResources.Employee, 
+--HumanResources.JobCandidate, используя LEFT OUTER JOIN
+SELECT e.BusinessEntityID
+	, j.[Resume]
+FROM HumanResources.Employee AS e
+LEFT OUTER JOIN HumanResources.JobCandidate AS j
+ON e.BusinessEntityID = j.BusinessEntityID
+WHERE j.[Resume] IS NOT NULL;
---
+++ Tasks/Task3/8.sql
@@ -0,0 +1,7 @@
+﻿--Показать список BusinessEntityID, которые содержатся в таблице Person.Person, 
+--но не содержатся в таблице HumanResources.Employee
+SELECT BusinessEntityID
+FROM Person.Person
+INTERSECT
+SELECT BusinessEntityID
+FROM HumanResources.Employee;
