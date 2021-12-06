3fbbddb1a20d858c4c389046aad11a14f332c68b
Прокопьева Екатерина Евгеньевна <katyaprokopyeva2000@mail.ru>
2021-12-06 21:04:23 +0300

    Выполнена лабораторная работа №3 #10

---
+++ Tasks/Task3/1.sql
@@ -0,0 +1,8 @@
+﻿--Показать количество товаров в корзине покупателя (Quantity) и поля Name, 
+--ProductNumber, ShoppingCartID из таблиц Production.Product, Sales.ShoppingCartItem.
+SELECT p.[Name]
+	, p.ProductNumber
+	, s.ShoppingCartID
+FROM Production.Product AS p
+INNER JOIN Sales.ShoppingCartItem AS s
+ON p.ProductID = s.ProductID;
